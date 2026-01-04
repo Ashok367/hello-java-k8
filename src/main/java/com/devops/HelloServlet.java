ec18aebb14333ef18d13e0da3f402876b55dd877
Ubuntu <ubuntu@ip-172-31-95-68.ec2.internal>
2026-01-04 21:38:10 +0000

    added files

---
+++ src/main/java/com/devops/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.devops;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello from Jenkins + Docker + Kubernetes</h1>");
+    }
+}
+
